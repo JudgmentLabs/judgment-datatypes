14a45a6a8b0203c3e96d6a8c7671fab37ec64b04
Aaryan Divate <divateaaryan@gmail.com>
2025-05-23 17:59:58 -0700

    Updated to just have data directory

--- __init__.py
+++ __init__.py
@@ -1,12 +1,19 @@
-# Import key components that should be publicly accessible
-from judgeval.clients import client, together_client
-from judgeval.judgment_client import JudgmentClient
-from judgeval.version_check import check_latest_version
-check_latest_version()
+from judgeval.data.example import Example, ExampleParams
+from judgeval.data.custom_example import CustomExample
+from judgeval.data.scorer_data import ScorerData, create_scorer_data
+from judgeval.data.result import ScoringResult, generate_scoring_result
+from judgeval.data.trace import Trace, TraceSpan, TraceUsage
+
 
 __all__ = [
-    # Clients
-    'client',
-    'together_client',
-    'JudgmentClient',
+    "Example",
+    "ExampleParams",
+    "CustomExample",
+    "ScorerData",
+    "create_scorer_data",
+    "ScoringResult",
+    "generate_scoring_result",
+    "Trace",
+    "TraceSpan",
+    "TraceUsage"
 ]
--- tracer/__init__.py
+++
@@ -1,3 +0,0 @@
-from judgeval.common.tracer import Tracer, wrap, TraceClient, TraceManagerClient
-
-__all__ = ["Tracer", "wrap", "TraceClient", "TraceManagerClient"]
