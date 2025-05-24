fa09d12708d19edc327106a1680b208642e542dc
Aaryan Divate <divateaaryan@gmail.com>
2025-05-24 16:27:49 -0700

    Remove specific init for judgeval

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,4 +1,3 @@
 from judgeval.data.datasets.dataset import EvalDataset
-from judgeval.data.datasets.eval_dataset_client import EvalDatasetClient
 
-__all__ = ["EvalDataset", "EvalDatasetClient"]
+__all__ = ["EvalDataset"]
